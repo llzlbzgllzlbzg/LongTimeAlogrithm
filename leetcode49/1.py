94bdd23cb7aad867f0defaf9b76fd5d63f87d02f
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-03-31 16:37:10 +0800

    leetcode49-improve

--- leetcode49/1.py
+++ leetcode49/1.py
@@ -7,6 +7,7 @@ class Solution:
         hashtable = {}
         for s in strs:
             sorted_s = ''.join(sorted(s))
+            # 这里用排序后的字符串做为哈希表的键，换成统计每个字符出现的次数可以更快一些
             if sorted_s in hashtable:
                 hashtable[sorted_s].append(s)
             else:
