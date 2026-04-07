9fd5db05bf5e1e24b8094f984bde65c926b7c04e
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-07 17:15:56 +0800

    leetcode483

---
+++ leetcode483/1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        p_count = [0] * 26
+        for i in p:
+            p_count[ord(i) - ord('a')] += 1
+        s_count = [0] * 26
+        result = []
+        if len(s) < len(p):
+            return result
+        for i in range(len(p) - 1):
+            s_count[ord(s[i]) - ord('a')] += 1
+        left, right = 0, len(p) - 1
+        while right < len(s):
+            s_count[ord(s[right]) - ord('a')] += 1
+            if s_count == p_count:
+                result.append(left)
+            s_count[ord(s[left]) - ord('a')] -= 1
+            left += 1
+            right += 1
+        return result
