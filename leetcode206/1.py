56ad87a2ba3f44d328ff251047ea8bcebe71deb1
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-11 15:39:08 +0800

    leetcode206

---
+++ leetcode206/1.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
