99ed10bb6a11a74c0f999752b996220f3ba04565
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-11 14:04:47 +0800

    leetcode73-improve

---
+++ leetcode73/2.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        flag_col0 = False
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                flag_col0 = True
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(len(matrix) - 1, -1, -1):
+                for j in range(1, len(matrix[0])):
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+                if flag_col0:
+                    matrix[i][0] = 0
