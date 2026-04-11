fed12721b1177577d658d40667386376eaaf4292
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-11 16:30:58 +0800

    leetcode142

---
+++ leetcode142/1.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        slow = head
+        fast = head
+        while True:
+            if not fast or not fast.next:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        ptr = head
+        while ptr != slow:
+            ptr = ptr.next
+            slow = slow.next
+        return ptr
