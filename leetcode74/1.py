6fd71ccd673c275d1850a10ab82aa1212d380498
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-15 16:23:44 +0800

    leetcode74

---
+++ leetcode74/1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        left = 0
+        right = len(matrix) * len(matrix[0])
+        while left < right:
+            mid = (left + right) // 2
+            r = mid // len(matrix[0])
+            c = mid % len(matrix[0])
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return False
