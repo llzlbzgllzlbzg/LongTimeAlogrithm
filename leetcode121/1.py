830df9c17dac74e7b7d48f8cad0bbd7a0bcd7e5a
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-21 19:50:16 +0800

    leetcode121

---
+++ leetcode121/1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        min_price = prices[0]
+        for price in prices:
+            ans = max(ans, price - min_price)
+            min_price = min(min_price, price)
+        return ans
