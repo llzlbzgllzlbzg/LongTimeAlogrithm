a265e4f12189e4c947a43c2eafe0511538b2d01f
llzlbzgllzlbzg <yaowentao2014@126.com>
2026-04-24 19:39:57 +0800

    leetcode169

---
+++ leetcode169/1.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        ans = 0
+        for num in nums:
+            if count == 0:
+                ans = num
+            if ans == num:
+                count += 1
+            else:
+                count -= 1
+        return ans
